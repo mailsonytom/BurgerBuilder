db4fa9218c8639037ba034f5d022a5b31fa1246b
Sony <mailsonytom@gmail.com>
2020-05-11 15:10:05 +0530

    Posting Signup data to db

--- src/Store/Actions/Auth.js
+++ src/Store/Actions/Auth.js
@@ -1,4 +1,4 @@
-
+import axios from 'axios';
 
 export const authStart = () => {
     return {
@@ -23,5 +23,20 @@ export const authFail = (error) => {
 export const auth = (email, password) => {
     return dispatch => {
         dispatch(authStart());
+        const AuthData = {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        };
+        console.log(AuthData)
+        axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAXTE8yOl88kPOEhJxuPjhQ9KIpTIqpHyY', AuthData)
+            .then(response => {
+                console.log(response);
+                dispatch(authSuccess(response.data))
+            })
+            .catch(err => {
+                console.log(err);
+                dispatch(authFail(err));
+            });
     };
 };
