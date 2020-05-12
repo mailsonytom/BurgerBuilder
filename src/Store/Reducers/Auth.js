6d8059cd23a9000ad87d4aaa75af44e37e05864e
Sony <mailsonytom@gmail.com>
2020-05-12 12:43:27 +0530

    Making switch case leaner

--- src/Store/Reducers/Auth.js
+++ src/Store/Reducers/Auth.js
@@ -8,33 +8,43 @@ const initialState = {
     loading: false
 };
 
+const authStart = (state, action) => {
+    return updatedObject(state, {
+        error: null,
+        loading: true
+    });
+}
+
+const authSuccess = (state, action) => {
+    return updatedObject(state, {
+        token: action.idToken,
+        userId: action.userId,
+        error: null,
+        loading: false
+    });
+}
+
+const authFail = (state, action) => {
+    return updatedObject(state, {
+        error: action.error,
+        loading: false
+    });;
+}
+
+const authLogout = (state, action) => {
+    return updatedObject(state, {
+        token: null,
+        userId: null
+    });
+}
+
 const reducer = (state = initialState, action) => {
     switch (action.types) {
-        case 'AUTH_START':
-            return updatedObject(state, {
-                error: null,
-                loading: true
-            });
-        case 'AUTH_SUCCESS':
-            return updatedObject(state, {
-                token: action.idToken,
-                userId: action.userId,
-                error: null,
-                loading: false
-            });
-        case 'AUTH_FAIL':
-            return updatedObject(state, {
-                error: action.error,
-                loading: false
-            });
-            
-        case 'AUTH_LOGOUT':
-            return updatedObject(state, {
-                token: null,
-                userId: null
-            });
-        default:
-            return state
+        case 'AUTH_START': return authStart(state, action);
+        case 'AUTH_SUCCESS': return authSuccess(state, action);
+        case 'AUTH_FAIL': return authFail(state, action);
+        case 'AUTH_LOGOUT': return authLogout(state, action);
+        default: return state
     }
 }
 
