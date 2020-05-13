7a562473311d0fde138b0148f023484417f55632
Sony <mailsonytom@gmail.com>
2020-05-13 13:11:09 +0530

    Guard routes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Builder from './Containers/Builder/Builder';
 import Layout from './Components/Layout/Layout';
 import Checkout from './Containers/Checkout/Checkout';
-import { Route, withRouter } from 'react-router';
+import { Route, withRouter, Switch, Redirect } from 'react-router';
 import Orders from './Containers/Orders/Orders';
 import Logout from './Containers/Authentication/Logout/Logout';
 import Auth from './Containers/Authentication/Auth';
@@ -16,24 +16,45 @@ class App extends Component {
   }
 
   render() {
+    let routes = (
+      <Switch>
+        <Route path="/auth" component={Auth} />
+        <Route path="/" exact component={Builder} />
+        <Redirect to="/" />
+      </Switch>
+    );
+
+    if (this.props.isAuthenticated) {
+      routes = (
+        <Switch>
+          <Route path="/" exact component={Builder} />
+          <Route path="/orders" component={Orders} />
+          <Route path="/logout" component={Logout} />
+          <Route path="/checkout" component={Checkout} />
+          <Redirect to="/" />
+        </Switch>
+      )
+    }
     return (
       <div>
         <Layout>
-          <Route path="/" exact component={Builder} />
-          <Route path="/orders" component={Orders} />
-          <Route path="/auth" component={Auth} />
-          <Route path="/logout" component={Logout} />
-          <Route path="/checkout" component={Checkout} />
+          {routes}
         </Layout>
       </div>
     );
   }
 }
 
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.token !== null
+  };
+};
+
 const mapDispatchToProps = dispatch => {
   return {
     onTryAutoSignup: () => dispatch(actions.authCheckState())
-  }
-}
+  };
+};
 
-export default withRouter(connect(null, mapDispatchToProps)(App));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
